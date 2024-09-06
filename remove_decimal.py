50aa23034a6bad4dc0c146f70002a9c0c8a3f76d
Isabela Santos Ramos <142419134+bbbelaramos@users.noreply.github.com>
2024-09-06 11:42:50 -0300

    Update remove_decimal.py

--- remove_decimal.py
+++ remove_decimal.py
@@ -1,3 +1,8 @@
 num = float(input("Choose a number: "))
 
-print("Your rounded numer is: {0:.0f}".format(num))
+print("Your entire portion number is: {0:.0f}".format(num))
+
+#second option
+import math
+num = float(input("Escolha um número: "))
+print("Sua porção inteira é: {}!".format(math.trunc(num)))
